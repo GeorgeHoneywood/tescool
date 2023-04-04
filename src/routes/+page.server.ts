3570e6297c6cf12a9fd00837af5c0ac0a7d7392b
GeorgeHoneywood <george.honeywood.2019@rhul.ac.uk>
2023-04-04 02:06:06 +0100

    feat: basics up and running

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,48 @@
+// credit to: https://stackoverflow.com/a/7228322
+function randomIntFromInterval(min: number, max: number) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export async function load({ params }) {
+    const resp = await fetch("https://www.tesco.com/groceries/en-GB/resources", {
+
+        "credentials": "include",
+        "headers": {
+            "Accept": "application/json",
+            "Accept-Language": "en-GB,en;q=0.5",
+            "content-type": "application/json",
+            "x-csrf-token": "fkQYjp05-cqNoNNSA04MVNkl3Q4ZD4RVfCPw",
+            "cookie": "_csrf=C4kyhDX5wl99qeq-5NBN2xyt"
+        },
+        "body": JSON.stringify({
+            "resources": [
+                {
+                    "type": "productsByCategory",
+                    "params": {
+                        "query": {
+                            "page": randomIntFromInterval(0, 100) // assuming 100 pages at least (there are currently ~250)
+                        },
+                        "superdepartment": "food-cupboard"
+                    }
+                }
+            ],
+            // "requiresAuthentication": false,
+        }),
+        "method": "POST",
+        "mode": "cors"
+    });
+
+    const data = await resp.json()
+    const pageSize = data.productsByCategory.data.results.pageInformation.pageSize;
+
+    const start = Math.min(
+        randomIntFromInterval(0, pageSize - 1),
+        pageSize - 6,
+    )
+
+    const items = data.productsByCategory.data.results.productItems.map(
+        (e: any) => e.product
+    ).slice(start, start + 5)
+
+    return { items };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,146 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let items = data.items;
+	let index = 0;
+	let item = items[0];
+
+	let guess: number | null = null;
+	let submitted = false;
+	let finished = false;
+
+	let score = 0;
+
+	const roundBrackets: Record<number, { text: string; color: string }> = {
+		5: { text: 'Dead on!', color: 'green' },
+		15: { text: 'Close!', color: 'blue' },
+		25: { text: 'Not bad!', color: 'orange' },
+		50: { text: 'Not too far off!', color: 'orange' },
+		Infinity: { text: "You're a disgrace!", color: 'red' }
+	};
+
+	let percentOff = 0;
+	let roundBracket = { text: "You're a disgrace!", color: 'red' };
+
+	const checkGuess = () => {
+		if (guess === null) {
+			return;
+		}
+		submitted = true;
+		percentOff = Math.abs(((guess - item.price) / item.price) * 100);
+		let key = Object.keys(roundBrackets).find((e) => {
+			return percentOff < +e;
+		});
+
+		roundBracket = roundBrackets[+(key ?? Infinity)];
+
+		console.log('guess', percentOff, roundBracket);
+		console.log(percentOff);
+		score += (100 - percentOff) * 10;
+		console.log('submit guess', score);
+	};
+
+	const focus = (el: HTMLElement) => {
+		el.focus();
+	};
+
+	const next = () => {
+		index++;
+		if (index >= items.length) {
+			finished = true;
+			return;
+		}
+		item = items[index];
+		guess = null;
+		submitted = false;
+	};
+</script>
+
+<main>
+	<h1>Tescool!</h1>
+	<p>Guess the price of the Tesco item</p>
+
+	{#if !finished}
+		<div class="item-box">
+			<span>Score: {score.toFixed(0)}/5000</span>
+			<img src={item.defaultImageUrl} alt="Image of {item.title}" />
+			<span class="name">{item.title}</span>
+
+			<div class="guess">
+				<span>Guess: £</span>
+				<input
+					class="guess-input"
+					type="number"
+					min="0.00"
+					max="10000.00"
+					step="0.01"
+					placeholder="0.23"
+					bind:value={guess}
+					on:keypress={(e) => {
+						if (e.key === 'Enter') {
+							!submitted ? checkGuess() : next();
+						}
+					}}
+					use:focus
+				/>
+				{#if !submitted}
+					<button on:click={() => checkGuess()} disabled={!guess}>Submit</button>
+				{:else}
+					<button on:click={() => next()}>Next</button>
+				{/if}
+			</div>
+
+			{#if submitted}
+				<div class="actual" style="color: {submitted ? roundBracket.color : 'black'}">
+					<span>Actual price:</span>
+
+					<span class="price">{item.price.toFixed(2)}</span>
+					<span>{percentOff.toFixed(0)}% off &mdash; {roundBracket.text}</span>
+				</div>
+			{/if}
+		</div>
+	{:else}
+		<h2>Round complete!</h2>
+		<p>Your score is {score.toFixed(0)}/5000</p>
+
+        <button on:click={() => window.location.reload()}>Play again!</button>
+	{/if}
+</main>
+
+<style>
+	main {
+		max-width: 600px;
+		margin: auto;
+	}
+
+	.item-box {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.item-box > * {
+		margin-bottom: 0.5rem;
+	}
+
+	.price::before {
+		content: '£';
+	}
+
+	img {
+		width: 250px;
+		height: 250px;
+	}
+	.name {
+		font-style: italic;
+	}
+	.guess-input {
+		max-width: 100px;
+	}
+
+    input, button {
+        padding: 4px;
+    }
+</style>
